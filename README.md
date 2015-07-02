22c2b26863949f61837af08556e79399b204aa3b
johann MARIE-REINE <johann.marie.reine@gmail.com>
2015-07-02 13:19:21 +0200

    Version Opérationnelle du web service

---
+++ README.md
@@ -0,0 +1,4 @@
+service
+=======
+
+A Symfony project created on June 19, 2015, 3:30 pm.
---
+++ src/Acme/BlogBundle/Controller/PageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Acme\BlogBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+
+class PageController extends FOSRestController {
+
+    public function getPageAction($id) {
+        return $this->container->get('doctrine.entity_manager')->getRepository('Page')->find($id);
+    }
+
+}
