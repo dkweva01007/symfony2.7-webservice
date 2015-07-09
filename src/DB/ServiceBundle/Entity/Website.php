a18b81990229ccfd46f280370309b1ce6dfa76cc
johann MARIE-REINE <johann.marie.reine@gmail.com>
2015-07-09 17:11:38 +0200

    Correction et installation de paquet non dev

--- src/DB/ServiceBundle/Entity/Website.php
+++ src/DB/ServiceBundle/Entity/Website.php
@@ -3,7 +3,7 @@
 namespace DB\ServiceBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use FOS\UserBundle\Model\User as BaseUser;
+use FOS\UserBundle\Entity\User as BaseUser;
 use Symfony\Component\Validator\Constraints as Assert;
 use JMS\Serializer\Annotation\ExclusionPolicy;
 use JMS\Serializer\Annotation\Expose;
