803e0e22687ff00689a3bc7c6493993414ef1408
johann MARIE-REINE <johann.marie.reine@gmail.com>
2015-07-09 18:01:16 +0200

    commit divers

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -2,10 +2,6 @@
 
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
-<<<<<<< HEAD
-=======
-use FOS\UserBundle\FOSUserBundle;
->>>>>>> no_security
 
 class AppKernel extends Kernel {
 
@@ -19,29 +15,17 @@ class AppKernel extends Kernel {
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-<<<<<<< HEAD
             new FOS\UserBundle\FOSUserBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new DB\ServiceBundle\DBServiceBundle()
-=======
-            new FOS\RestBundle\FOSRestBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle(),
-            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
-            new DB\ServiceBundle\DBServiceBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
->>>>>>> no_security
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-<<<<<<< HEAD
-=======
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
->>>>>>> no_security
         }
 
         return $bundles;
